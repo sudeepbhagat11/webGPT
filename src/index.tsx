0cae9216714ee109493dc736484afc91e9637e5f
Sudeep Bhagat <sudeepbhagat456@gmail.com>
2024-04-11 00:40:07 +0530

    Latest Commit

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,10 @@ connectdb();
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/api", (req, res) => {
