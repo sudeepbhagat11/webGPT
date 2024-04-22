d004298739e7f1c29c1cbf35045b576641439840
Sudeep Bhagat <130081182+sudeepbhagat11@users.noreply.github.com>
2024-04-22 19:07:27 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 config();
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://web-gpt-flax.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
